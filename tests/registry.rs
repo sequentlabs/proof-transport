030cd8ab7c6125d3ec8712f81c8fb9e52fc8611a
sequentlabs <mark@mazlo.io>
2025-09-26 04:23:11 -0300

    Update registry.rs

--- tests/registry.rs
+++ tests/registry.rs
@@ -1,20 +1,25 @@
-use std::collections::HashSet;
 use proof_transport::registry::{Registry, RuleId, TimeSlice};
+use std::collections::HashSet;
 
 #[test]
 fn registry_enabled_at_works() {
     let reg = Registry {
         times: vec![
-            TimeSlice { t: 0, enabled_rules: vec![RuleId::Id, RuleId::Cut] },
-            TimeSlice { t: 1, enabled_rules: vec![RuleId::Id] },
+            TimeSlice {
+                t: 0,
+                enabled_rules: vec![RuleId::Id, RuleId::Cut],
+            },
+            TimeSlice {
+                t: 1,
+                enabled_rules: vec![RuleId::Id],
+            },
         ],
     };
 
-    let at0: HashSet<_> = reg.enabled_at(0);
-    assert!(at0.contains(&RuleId::Cut));
-    assert!(at0.contains(&RuleId::Id));
+    let at0 = reg.enabled_at(0);
+    let at1 = reg.enabled_at(1);
 
-    let at1: HashSet<_> = reg.enabled_at(1);
-    assert!(!at1.contains(&RuleId::Cut));
-    assert!(at1.contains(&RuleId::Id));
+    assert!(at0.contains(&RuleId::Id));
+    assert!(at0.contains(&RuleId::Cut));
+    assert_eq!(at1, HashSet::from([RuleId::Id]));
 }
