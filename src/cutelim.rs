1e67af2966c7a26d75c1534043006e29e031d3da
sequentdev <mark@mazlo.io>
2025-09-25 23:32:22 -0300

    Create cutelim.rs
    feat(cutelim): add cut-elimination stub (identity)

---
+++ src/cutelim.rs
@@ -0,0 +1,7 @@
+use crate::ast::Proof;
+
+/// placeholder: return identity until real algorithm is implemented
+pub fn cut_eliminate_all(p: &Proof) -> Proof {
+    // TODO: implement cut-elimination
+    p.clone()
+}
