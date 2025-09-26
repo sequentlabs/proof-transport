8cdbb4683a02044ac89459c549eb98809661e2ba
sequentlabs <mark@mazlo.io>
2025-09-26 04:22:33 -0300

    Update examples.rs

--- tests/examples.rs
+++ tests/examples.rs
@@ -1,16 +1,17 @@
-use std::fs;
 use serde_json::Value;
+use std::fs;
 
 #[test]
 fn load_example_proofs() {
+    let manifest = env!("CARGO_MANIFEST_DIR");
     let files = [
         "examples/proof_with_cut.json",
         "examples/proof_cut_eliminated.json",
         "examples/R.json",
     ];
-
     for f in files {
-        let data = fs::read_to_string(f).expect("file should exist");
+        let path = format!("{}/{}", manifest, f);
+        let data = fs::read_to_string(&path).expect("file should exist");
         let _: Value = serde_json::from_str(&data).expect("valid JSON");
     }
 }
