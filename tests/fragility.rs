515747139f9ca3695fd050794e4933f879315864
sequentlabs <mark@mazlo.io>
2025-09-26 05:07:54 -0300

    Update fragility.rs

--- tests/fragility.rs
+++ tests/fragility.rs
@@ -4,8 +4,6 @@ use std::fs::File;
 
 #[test]
 fn fragility_score_nonzero_on_cut_proof() {
-    let manifest = env!("CARGO_MANIFEST_DIR");
-    let path = format!("{}/examples/proof_with_cut.json", manifest);
-    let p: Proof = from_reader(File::open(path).expect("open")).expect("decode");
-    assert!(fragility_score(&p) >= 1);
+    let p: Proof = from_reader(File::open("examples/proof_with_cut.json").unwrap()).unwrap();
+    assert!(fragility_score(&p) > 1);
 }
