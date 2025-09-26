91f0de8c55089411f695bdcbf32a06a2bf49f0e6
sequentlabs <mark@mazlo.io>
2025-09-26 04:22:44 -0300

    Update fragility.rs

--- tests/fragility.rs
+++ tests/fragility.rs
@@ -1,9 +1,11 @@
 use proof_transport::{ast::Proof, frag::fragility_score};
-use std::fs::File;
 use serde_json::from_reader;
+use std::fs::File;
 
 #[test]
 fn fragility_score_nonzero_on_cut_proof() {
-    let p: Proof = from_reader(File::open("examples/proof_with_cut.json").unwrap()).unwrap();
+    let manifest = env!("CARGO_MANIFEST_DIR");
+    let path = format!("{}/examples/proof_with_cut.json", manifest);
+    let p: Proof = from_reader(File::open(path).expect("open")).expect("decode");
     assert!(fragility_score(&p) >= 1);
 }
