e8e659b41bea5f0438f427f9a6ec96a66feccd24
sequentlabs <mark@mazlo.io>
2025-09-26 02:13:41 -0300

    Update success.rs
    tests: add extremely small API-export test

--- tests/success.rs
+++ tests/success.rs
@@ -1,6 +1,7 @@
-// tests/success.rs
-
 #[test]
-fn always_passes() {
-    assert_eq!(2 + 2, 4);
+fn lib_exports_compile() {
+    let _ = proof_transport::frag::fragility_score
+        as fn(&proof_transport::ast::Proof) -> u64;
+    let _ = proof_transport::validator::validate_local_wf
+        as fn(&proof_transport::ast::Proof) -> anyhow::Result<()>;
 }
