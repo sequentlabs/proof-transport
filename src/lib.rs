f4f33bb3d5b99a346bcf296dd84231c9a7a35fc1
sequentlabs <mark@mazlo.io>
2025-09-27 01:19:30 -0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,14 @@ pub mod cutelim;
 pub mod frag;
 pub mod registry;
 pub mod validator;
+pub mod transport;
 
-// Re-export key types and functions so downstream crates & tests can use directly.
+// Re-export key types and functions so downstream crates & tests can use directly
 pub use ast::*;
 pub use cutelim::{cut_eliminate_all, cut_eliminate_root};
 pub use frag::fragility_score;
 pub use registry::*;
+pub use transport::{
+    fragility_delta, transport, transport_simple, transport_with_cert, Certificate,
+};
 pub use validator::validate_local_wf;
